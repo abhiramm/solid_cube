e2055e7abf89c31332c9754f9086153d6a68a91e
Abhirama sundram <abhiramasundram2007@gmail.com>
2013-08-08 13:17:47 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkingSlate::Application.config.secret_token = '73a1fdec0c08cfaba4c1b3110b1b256ba33cf625ea24072ab4fdd151fc0f1c6da8221e1dd4f6fb41620d8690da4a11ff4100379f0f542cba1d672b893546e9f4'
